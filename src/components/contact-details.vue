e321656457bb85332d9304532022ce50d8d2d7cc
Ron Shitrit <ronshitrit456@gmail.com>
2022-11-09 19:50:13 +0200

    style update

--- src/components/contact-details.vue
+++ src/components/contact-details.vue
@@ -4,18 +4,25 @@
     <div class="contact-info-box">
       <h1>INFO & CONTACT</h1>
       <div class="info-box">
+        <p>Currently a visual communication student at Bezalel Academy.</p>
         <p>
-          Currently a visual communication student at Bezalel Academy.
           Experienced in various freelancing works, and 2 years as a graphic
-          designer for the Israeli Navy. Fluent in Procreate, Figma, and Adobe
-          programs such as Photoshop, Illustrator, InDesign, Lightroom, After
-          Effects and Premiere.
+          designer for the Israeli Navy.
+        </p>
+        <p>
+          Fluent in Procreate, Figma, and Adobe programs such as Photoshop,
+          Illustrator, InDesign, Lightroom, After Effects and Premiere.
         </p>
         <div class="contact-info-list">
-          <img src="../assets/imgs/meee.png" />
-          <p>Phone: +972 54-249-8868</p>
-          <p>E-Mail: maya97abr@gmail.com</p>
-          <icon-base icon="insta"></icon-base>
+          <div>
+            <img src="../assets/imgs/meee.png" />
+            <div>
+              <p>Phone: +972 54-249-8868</p>
+              <p>E-Mail: maya97abr@gmail.com</p>
+            </div>
+          </div>
+          <img src="../assets/imgs/image 2.png" alt="" />
+          <!-- <icon-base icon="insta"></icon-base> -->
         </div>
       </div>
     </div>
@@ -23,6 +30,6 @@
 </template>
 
 <script>
-import IconBase from "./icon-base.vue";
-export default { components: { IconBase } };
+// import IconBase from "./icon-base.vue";
+export default { components: {} };
 </script>
